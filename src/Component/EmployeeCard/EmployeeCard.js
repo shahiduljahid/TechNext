c477e1f98324ddd6e94b7d7327bbac8b948187b6
shahiduljahid <68829960+shahiduljahid@users.noreply.github.com>
2021-08-17 07:19:07 +0800

    table added

--- src/Component/EmployeeCard/EmployeeCard.js
+++ src/Component/EmployeeCard/EmployeeCard.js
@@ -1,78 +1,106 @@
-import React from 'react';
-import "./EmployeeCard.css"
+import React, { useMemo } from "react";
+import "./EmployeeCard.css";
+import { useTable, usePagination } from "react-table";
+import { COLUMNS } from "./columns";
 
-const EmployeeCard = ({totalEmployees}) => {
-    return (
-        <div className="row">
-        <div className="px-5 py-5">
-          <table className="table table-borderless h7 rounded">
-            <thead className=" p-1 ">
-              <tr>
-                <th className="text-secondary serial-number " scope="col">
-                  sr.no
-                </th>
-                <th className="text-secondary firstName" scope="col">
-                  FirstName 
-                </th>
-                <th className="text-secondary lastName" scope="col">
-                  Last Name
-                </th>
-                <th className="text-secondary email" scope="col">
-                  Email
-                </th>
+const EmployeeCard = ({ totalEmployees }) => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => totalEmployees, [totalEmployees]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    headerGroups,
+    page,
+    pageOptions,
+    state,
+
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize: 5 },
+    },
+    usePagination
+  );
+  const { pageIndex } = state;
+
+  return (
+    <div className="row">
+      <div className="px-5 py-5">
+        <table
+          className="table table-borderless h7 rounded"
+          {...getTableProps()}
+        >
+          <thead>
+            {headerGroups.map((headergroup) => (
+              <tr {...headergroup.getHeaderGroupProps()}>
+                {headergroup.headers.map((column) => (
+                  <th
+                    className="text-secondary "
+                    scope="col"
+                    {...column.getHeaderProps()}
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
                 <th className="text-secondary " scope="col">
-                  Send Mail
+                  Mail
                 </th>
-               
               </tr>
-            </thead>
-            {totalEmployees.map((apm, index) => {
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
               return (
-                <tbody>
-                  <tr>
-                    <td
-                      style={{ fontWeight: "bold", color: "#0A0A0A" }}
-                      className="text-weight-bold serial-number"
-                    >
-                      {index + 1}
-                    </td>
-                    <td
-                      style={{ fontWeight: "bold", color: "#0A0A0A" }}
-                      className="text-weight-bold firstName "
-                    >
-                      {apm.firstName}
-                    </td>
-                    <td
-                      style={{ fontWeight: "bold", color: "#0A0A0A" }}
-                      className="text-weight-bold lastName"
-                    >
-                      {apm.lastName}
-                    </td>
-  
-                    <td
-                      style={{ fontWeight: "bold", color: "#0A0A0A" }}
-                      className="text-weight-bold email"
-                    >
-                      {apm.email}
-                    </td>
-                    
-                    <td className="text-weight-bold">
-                      {" "}
-                      <button
-                        style={{ fontWeight: "bold" }}
-                        className="btn text-light mailBtn text-bold btn-color"
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td
+                        style={{ fontWeight: "bold", color: "#0A0A0A" }}
+                        className="text-weight-bold "
+                        {...cell.getCellProps()}
                       >
-                       Send
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
+                        {cell.render("Cell")}
+                      </td>
+                    );
+                  })}
+                  <td className="text-weight-bold">
+                    {" "}
+                    <button
+                      style={{ fontWeight: "bold" }}
+                      className="btn text-light mailBtn text-bold btn-color"
+                    >
+                      Send
+                    </button>
+                  </td>
+                </tr>
               );
             })}
-          </table>
+          </tbody>
+        </table>
+        <div className="text-center">
+          
+          <button className="me-2 btn btn-primary" onClick={() => previousPage()} disabled={!canPreviousPage}>
+            Previous
+          </button>
+          <span>
+            <strong>{' '}</strong>
+            {pageIndex + 1} of {pageOptions.length}  <strong>{' '}</strong>
+          </span>
+          <button className="ms-2 btn btn-secondary" onClick={() => nextPage()} disabled={!canNextPage}>
+            Next
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default EmployeeCard;
---
+++ src/Component/EmployeeCard/columns.js
@@ -0,0 +1,14 @@
+export const COLUMNS =[
+    {
+        Header:'firstName',
+        accessor:'firstName'
+    },
+    {
+        Header:'lastName',
+        accessor:'lastName'
+    },
+    {
+        Header:'email',
+        accessor:'email'
+    }
+]
