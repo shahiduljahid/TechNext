086f11f12a9d2006f4c8b8d7a1b211e7d4bac4b5
shahiduljahid <68829960+shahiduljahid@users.noreply.github.com>
2021-12-15 00:18:06 +0800

    colums added

---
+++ src/Component/RecruiteForm/columns.js
@@ -0,0 +1,47 @@
+export const COLUMNS = [
+  {
+    Header: "NAME",
+    accessor: "NAME",
+  },
+  {
+    Header: "PHONE_NUMBER",
+    accessor: "PHONE_NUMBER",
+  },
+  {
+    Header: "LICENSE",
+    accessor: "LICENSE",
+  },
+  {
+    Header: "TERMINAL_ALLOWED",
+    accessor: "TERMINAL_ALLOWED",
+  },
+  {
+    Header: "SMS_LICENSE",
+    accessor: "SMS_LICENSE",
+  },
+  {
+    Header: "SMS_STATUS",
+    accessor: "SMS_STATUS",
+  },
+  {
+    Header: "SMS_DEADLINE_DATE",
+    accessor: "SMS_DEADLINE_DATE",
+  },
+
+  {
+    Header: "SMS_COST",
+    accessor: "SMS_COST",
+  },
+  {
+    Header: "LICENSE_COST",
+    accessor: "LICENSE_COST",
+  },
+  {
+    Header: "UID",
+    accessor: "UID",
+  },
+  {
+    Header: "ACTIVATION_DATE",
+    accessor: "ACTIVATION_DATE",
+  },
+];
